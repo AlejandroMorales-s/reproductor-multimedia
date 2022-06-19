075aa6387dc31323a1d8b9d4cd709ae1fbb76337
Alejandro <alejandrom.am33@gmail.com>
2022-06-19 18:25:15 -0500

    add: navbar styles and icons

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import css from'./main.scss';
+import scss from'./main.scss';
 import {router} from './router/index.routes';
-
+navbar();
 router(window.location.hash);
 window.addEventListener('hashchange', () => {
     router(window.location.hash);
